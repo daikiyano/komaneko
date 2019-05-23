55e8619a4de571ed1403764ed7d78e0562fddc8e
矢野大樹 <>
2019-05-23 23:15:12 +0900

    Add validation error

--- companyblog/blog_posts/forms.py
+++ companyblog/blog_posts/forms.py
@@ -4,15 +4,15 @@ from wtforms.validators import DataRequired
 from flask_wtf.file import FileField,FileAllowed
 
 class BlogPostForm(FlaskForm):
-    title = StringField('イベント名',validators=[DataRequired()])
-    text = TextAreaField('イベント詳細',validators=[DataRequired()])
+    title = StringField('イベント名',validators=[DataRequired("イベント名を入力してください。")])
+    text = TextAreaField('イベント詳細',validators=[DataRequired("イベント詳細を入力してください。")])
     image = FileField('イメージ画像',validators=[FileAllowed(['jpg','png'])])
-    organizer = StringField('主催者',validators=[DataRequired()])
-    place = StringField('場所',validators=[DataRequired()])
-    entry = StringField('参加条件',validators=[DataRequired()])
-    way = StringField('参加方法',validators=[DataRequired()])
-    cost = StringField('参加費用',validators=[DataRequired()])
-    contact = StringField('問い合わせ',validators=[DataRequired()])
+    organizer = StringField('主催者',validators=[DataRequired("主催者を入力してください。")])
+    place = StringField('場所',validators=[DataRequired("場所を入力してください。")])
+    entry = StringField('参加条件',validators=[DataRequired("参加条件を入力してください。")])
+    way = StringField('参加方法',validators=[DataRequired("参加方法を入力してください。")])
+    cost = StringField('参加費用',validators=[DataRequired("参加費用を入力してください。")])
+    contact = StringField('問い合わせ',validators=[DataRequired("問い合わせ先を入力してください。")])
     image = FileField('イメージ画像',validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField("新規イベントを投稿する")
 
