2e62ceca83c9e0d130f3adf3f1a9b3ad6165b6a0
矢野大樹 <>
2019-06-30 21:07:08 +0900

    no message

--- manage.py
+++ manage.py
@@ -10,3 +10,12 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+
+    # migrate database to latest revision
+    upgrade()
